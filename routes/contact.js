022029a8fdc6b0626ece2ca994cf72d7b5133c72
Soo <gsj2464@gmail.com>
2019-02-12 13:48:25 -0500

    added Express CRUD

--- routes/contact.js
+++ routes/contact.js
@@ -1,51 +1,128 @@
-let express = require('express');
+let express = require("express");
+
 let router = express.Router();
-let mongoose = require('mongoose');
 
-// create a reference to the db schema
-let contactModel = require('../models/contact');
+let mongoose = require("mongoose");
 
-/* GET Contact List page - READ Operation */
-router.get('/', (req, res, next) => {
-    contactModel.find((err, contactList) => {
-        if(err) {
-            return console.error(err);
-        }
-        else {            
-            res.render('contacts/index', {
-                title: 'Contact List',
-                contactList: contactList
-            });
-        }
-    });
+// create a reference to the contact model
+
+let contactModel = require("../models/contact");
+
+/* GET contact list page - READ */
+
+router.get("/", (req, res, next) => {
+  contactModel.find((err, contactList) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      //console.log(contactList);
+
+      res.render("contacts/index", {
+        title: "Contact List",
+
+        contactList: contactList
+      });
+    }
+  });
 });
 
-/* GET Route for the Add page
-   this will display the Add */
-router.get('/add', (req, res ,next) => {
-    res.render('contacts/add', {
-        title: 'Add New Contact',
-    });
+// GET the Add page for the Contact-List
+
+router.get("/add", (req, res, next) => {
+  res.render("contacts/add", {
+    title: "Add a New Contact"
+  });
 });
 
-/* POST Route for processing the Add page */
-router.post('/add', (req, res, next) => {
-    let newContact = contactModel({
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName,
-        "age": req.body.age
-    });
+// POST - process the Add page for the Contact-List
 
-    contactModel.create(newContact, (err, contactModel) => {
-        if(err){
+router.post("/add", (req, res, next) => {
+  let newContact = contactModel({
+    firstName: req.body.FirstNameTextField,
+
+    lastName: req.body.LastNameTextField,
+
+    age: req.body.AgeTextField
+  });
+
+  contactModel.create(newContact, (err, contact) => {
+    if (err) {
+      console.log(err);
+
+      res.end(err);
+    } else {
+      // takes the user back to the contact-list page
+
+      res.redirect("/contact-list");
+    }
+  });
+});
+
+/* GET the Edit Page */
+
+router.get("/edit/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  contactModel.findById(id, (err, contactObject) => {
+    if (err) {
+      console.log(err);
+
+      res.end(err);
+    } else {
+      // show the edit view
+
+      res.render("contacts/edit", {
+        title: "Edit Contact",
+
+        contact: contactObject
+      });
+    }
+  });
+});
+
+/* POST - process the information from Edit page */
+
+router.post("/edit/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  let updatedContact = contactModel({
+    _id: id,
+
+    firstName: req.body.FirstNameTextField,
+
+    lastName: req.body.LastNameTextField,
+
+    age: req.body.AgeTextField
+  });
+
+  contactModel.update({ _id: id }, updatedContact, err => {
+    if (err) {
+      console.log(err);
+
+      res.end(err);
+    } else {
+      // refresh the contact-list
+
+      res.redirect("/contact-list");
+    }
+  });
+});
+
+/* GET request to perform the delete action */
+router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    contactModel.remove({_id: id}, (err) => {
+        if(err)
+        {
             console.log(err);
             res.end(err);
         }
         else {
-            // refresh the contact list
-            res.redirect('/contact-list');
+            // refresh the contact-list
+            res.redirect("/contact-list");
         }
     });
-})
+});
 
 module.exports = router;
