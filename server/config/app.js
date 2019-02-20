c461d7ef977383d08ce54cd8b36b8afb6e1456de
Soo <gsj2464@gmail.com>
2019-02-19 19:45:51 -0500

    created User Model

--- server/config/app.js
+++ server/config/app.js
@@ -1,8 +1,17 @@
+// moduels for node and express
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+
+// modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
 
 // database setup
 let mongoose = require('mongoose');
@@ -32,6 +41,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+// set up express-session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+// initialize flash
+app.use(flash());
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport user configuration
+
+// create a User model
+
+// implement a User authentication strategy
+
+// serialize and deserialize the User info
 
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
