c218eb66b275a786d1234eda5e1dcee6c4e106ce
Soo <gsj2464@gmail.com>
2019-02-20 19:27:24 -0500

    enabled user authorization

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -2,29 +2,39 @@ let express = require("express");
 
 let router = express.Router();
 
+let passport = require('passport');
+
 let contactController = require("../controllers/contact");
+
+function requireAuth(req, res, next) {
+    // check if the user is logged in
+    if(!req.isAuthenticated() || (req.user.username != "admin")) {
+        return res.redirect('/login');
+    }
+    next();
+}
 
 /* GET contact list page - READ */
 
-router.get("/", contactController.displayContactList);
+router.get("/", requireAuth, contactController.displayContactList);
 
 // GET the Add page for the Contact-List
 
-router.get("/add", contactController.displayAddPage);
+router.get("/add", requireAuth, contactController.displayAddPage);
 
 // POST - process the Add page for the Contact-List
 
-router.post("/add", contactController.processAddPage);
+router.post("/add", requireAuth, contactController.processAddPage);
 
 /* GET the Edit Page */
 
-router.get("/edit/:id", contactController.displayEditPage);
+router.get("/edit/:id", requireAuth, contactController.displayEditPage);
 
 /* POST - process the information from Edit page */
 
-router.post("/edit/:id", contactController.processEditPage);
+router.post("/edit/:id", requireAuth, contactController.processEditPage);
 
 /* GET request to perform the delete action */
-router.get('/delete/:id', contactController.performDelete);
+router.get('/delete/:id', requireAuth, contactController.performDelete);
 
 module.exports = router;
